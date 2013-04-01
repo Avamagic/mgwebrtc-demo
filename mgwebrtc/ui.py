4fb96122dee8e7e6fafee7d4966c68f4444014bd
Ron Huang <ronhuang@avamagic.com>
2013-04-01 11:45:08 +0800

    Further refine rooms.html

--- mgwebrtc/ui.py
+++ mgwebrtc/ui.py
@@ -2,5 +2,17 @@ from flask import render_template
 from mgwebrtc import app
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def rooms():
+    return render_template('rooms.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/conf')
+def conf():
+    return render_template('conf.html')
+
+@app.route('/events')
+def events():
+    return ''
